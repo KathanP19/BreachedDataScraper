f0e0068fe3cbe07e537fd0f859e3bfabfcbdd59f
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-11-24 22:18:26 +0500

    Add files via upload

---
+++ url_writer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "[+] Copy and Paste Url In List"
+echo -ne "[+] Provide List Name: "
+read url
+wget -i $url
+echo "";
+echo "";
+echo "";
+echo -e "[+] URLS have been written in file: $url "
