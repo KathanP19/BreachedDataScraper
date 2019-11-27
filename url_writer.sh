f5932dde20b17531b7ec0d27a7e52635229c4027
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-11-28 03:07:20 +0500

    Update url_writer.sh

--- url_writer.sh
+++ url_writer.sh
@@ -7,8 +7,6 @@ wget -i $url
 echo "";
 echo "";
 echo -e "[+] URLS have been written in file "
-echo -ne "[+] Enter data search term to extract from downloaded files: "
-read term
-grep -e '$term' * > download_result
-echo -e "[+] Data is extracted, see download_result file"
+echo "";
+./extractor.sh
 echo "";
