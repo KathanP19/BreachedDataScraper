b3fb8eb06c4f75a94eb2ec5549166950c8e73098
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-11-28 02:47:10 +0500

    Update url_writer.sh

--- url_writer.sh
+++ url_writer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo -e "[+] Copy and Paste Url In List"
-echo -ne "[+] Provide List Name: "
+echo "";
+echo -e "[+] Before use, Copy/Paste Urls In File"
+echo -ne "[+] Enter Url File Name: "
 read url
 wget -i $url
 echo "";
 echo "";
-echo "";
-echo -e "[+] URLS have been written from file: $url "
-echo "";
+echo -e "[+] URLS have been written in file "
+echo -ne "[+] Enter data search term to extract from downloaded files: "
+read term
+grep -e '$term' * > download_result
+echo -e "[+] Data is extracted, see download_result file"
 echo "";
